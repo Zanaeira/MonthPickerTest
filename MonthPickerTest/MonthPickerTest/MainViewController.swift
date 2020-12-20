bc62acf363bbaf5d6f28ba9935b187def9661b46
Zanaeira <suhayl.ahmed@icloud.com>
2020-12-20 22:39:34 +0000

    Base project

---
+++ MonthPickerTest/MonthPickerTest/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  MonthPickerTest
+//
+//  Created by Suhayl Ahmed on 20/12/2020.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+}
